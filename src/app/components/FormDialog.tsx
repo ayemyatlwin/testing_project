4f52709ef330a7f7c3b0bdfbb7c9b6ebebd4db14
Hannah <108206521+ayemyatlwin@users.noreply.github.com>
2024-02-22 14:05:36 +0630

    testing

---
+++ src/app/components/FormDialog.tsx
@@ -0,0 +1,69 @@
+"use client";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { FormControl, FormLabel, Stack } from "@mui/material";
+import { DateRangePicker } from "rsuite";
+import "rsuite/dist/rsuite.css";
+
+export default function FormDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open form dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: "form",
+          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries((formData as any).entries());
+            const email = formJson.email;
+            console.log(email);
+            handleClose();
+          },
+        }}
+      >
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <Stack spacing={1} direction="row">
+            <FormControl>
+              <FormLabel> Date Range (from - to) </FormLabel>
+              <DateRangePicker
+                id="rs_picker_popup"
+                appearance="default"
+                placement="topStart"
+                preventOverflow={true}
+                placeholder="dd/mm/yyyy ~ dd/mm/yyyy"
+                format="dd/MM/yyy"
+                
+              />
+            </FormControl>
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
